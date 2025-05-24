70a2ef1635e0d2035e9536b10c93da3d65543989
Eslam <ea450424@gmail.com>
2025-05-24 21:42:50 +0200

    videoDetails page

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { hostname: 'ea-snapscreen.b-cdn.net', protocol: 'https', port: '', pathname: '/**' },
+      { hostname: '*', protocol: 'https', port: '', pathname: '/**' },
     ]
   }
 };
